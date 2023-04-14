e68478001cd049fe6bd5d19a74753c81af279002
Aakash Shukla <shukla.aa@northeastern.edu>
2023-04-13 23:49:45 -0400

    Added some more resolvers

--- GraphQL.API/Schema/CourseType.cs
+++ GraphQL.API/Schema/CourseType.cs
@@ -11,6 +11,8 @@
         public Guid Id { get; set; }
         public string Name { get; set; }
         public Subject Subject { get; set; }
+
+        [GraphQLNonNullType]
         public InstructorType Instructor { get; set; }
         public IEnumerable<StudentType> Students { get; set; }
     }
--- GraphQL.API/Schema/Query.cs
+++ GraphQL.API/Schema/Query.cs
@@ -4,31 +4,48 @@ namespace GraphQL.API.Schema
 {
     public class Query
     {
-        [GraphQLDeprecated("This query is deprecated")]
-        public string Instructions => "Some Message";
+        private readonly Faker<InstructorType> _instructorFaker;
+        private readonly Faker<StudentType> _studentFaker;
+        private readonly Faker<CourseType> _courseFaker;
 
-        public IEnumerable<CourseType> GetCourses()
+        public Query()
         {
-            var instructorFaker = new Faker<InstructorType>()
+            _instructorFaker = new Faker<InstructorType>()
                 .RuleFor(x => x.Id, y => Guid.NewGuid())
                 .RuleFor(x => x.FirstName, y => y.Name.FirstName())
                 .RuleFor(x => x.LastName, y => y.Name.LastName())
                 .RuleFor(x => x.Salary, y => y.Random.Double(0, 100000));
 
-            var studentFaker = new Faker<StudentType>()
+            _studentFaker = new Faker<StudentType>()
                 .RuleFor(x => x.Id, y => Guid.NewGuid())
                 .RuleFor(x => x.FirstName, y => y.Name.FirstName())
                 .RuleFor(x => x.LastName, y => y.Name.LastName())
                 .RuleFor(x => x.GPA, f => f.Random.Double(1, 4));
 
-            var courseFaker = new Faker<CourseType>()
+            _courseFaker = new Faker<CourseType>()
                 .RuleFor(x => x.Id, y => Guid.NewGuid())
                 .RuleFor(x => x.Name, y => y.Name.JobTitle())
                 .RuleFor(x => x.Subject, y => y.PickRandom<Subject>())
-                .RuleFor(x => x.Instructor, y => instructorFaker.Generate())
-                .RuleFor(x => x.Students, y => studentFaker.Generate(3));
-            var courses = courseFaker.Generate(5);
+                .RuleFor(x => x.Instructor, y => _instructorFaker.Generate())
+                .RuleFor(x => x.Students, y => _studentFaker.Generate(3));
+        }
+
+        [GraphQLDeprecated("This query is deprecated")]
+        public string Instructions => "Some Message";
+
+        public IEnumerable<CourseType> GetCourses()
+        {
+            
+            var courses = _courseFaker.Generate(5);
             return courses;
+        }
+
+        public async Task<CourseType> GetCourseByIdAsync(Guid id)
+        {
+            await Task.Delay(1000);
+            CourseType course = _courseFaker.Generate();
+            course.Id = id;
+            return course;
         }
     }
 }
